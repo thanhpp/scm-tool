f1bb5f0bcacaf969e421ec3a3249459c5b62aae1
thanhpp <thanhphanphu18@gmail.com>
2022-06-06 11:52:12 +0700

    refactor: 🔥 Remove ent
    - remove entgo files
    
    Author: thanhphanphu18@gmail.com

--- cmd/scmsrv/main.go
+++ cmd/scmsrv/main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"context"
-
 	_ "github.com/lib/pq"
-	"github.com/thanhpp/scm/ent"
 )
 
 func main() {
-	setupDB()
-}
 
-func setupDB() {
-	client, err := ent.Open(
-		"postgres",
-		"host=localhost port=5432 user=user dbname=scm password=password sslmode=disable",
-	)
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-
-	if err := client.Schema.Create(context.Background()); err != nil {
-		panic(err)
-	}
 }
