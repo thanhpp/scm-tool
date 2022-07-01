a626b9c3e33ddc9d3e39f194bcb79a6ad3b04772
thanhpp <thanhphanphu18@gmail.com>
2022-07-01 11:41:10 +0700

    feat: ✨ Add cors
    Author: thanhpp <thanhphanphu18@gmail.com>
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/thanhpp/scm
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.8.1
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/pkg/errors v0.9.1
--- internal/scmsrv/infra/port/httpsv/httpsv.go
+++ internal/scmsrv/infra/port/httpsv/httpsv.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 	"github.com/thanhpp/scm/internal/scmsrv/app"
@@ -68,6 +69,17 @@ func (s HTTPServer) newRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 24 * time.Hour,
+	}))
 
 	// init
 	importTicketCtrl := ctrl.NewImportTicket(s.app.ImportTicketHandler)
